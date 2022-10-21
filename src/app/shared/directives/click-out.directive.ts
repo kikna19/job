a338c5154473f7b15de9d37b2708ab74a4c3ede0
kikna19 <vazhiko199@gmail.com>
2022-10-21 17:26:56 +0400

    dropdown

---
+++ src/app/shared/directives/click-out.directive.ts
@@ -0,0 +1,26 @@
+﻿import {Directive, ElementRef, EventEmitter, HostListener, Output} from '@angular/core';
+
+@Directive({
+  selector: '[appClickOut]'
+})
+export class ClickOutDirective {
+  @Output() toggle: EventEmitter<any> = new EventEmitter();
+  @Output() close: EventEmitter<any> = new EventEmitter();
+
+  constructor(
+    private el: ElementRef
+  ) {
+  }
+
+  @HostListener('document:click', ['$event.target']) closeEl(target: any) {
+    const clickInside = this.el.nativeElement.parentElement.childNodes[0].contains(target);
+    const clickOut = this.el.nativeElement.parentElement.childNodes[1].contains(target);
+
+    if (clickInside)
+      this.toggle.emit();
+
+    if (!clickInside && !clickOut)
+      this.close.emit();
+  }
+
+}
